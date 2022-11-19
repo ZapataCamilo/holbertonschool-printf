25069cff9e01188f6ed580624e6f442be1380603
ZapatCamilo <zapatacamilo86@gmail.com>
2022-11-19 11:46:37 -0800

    operation

--- operation.c
+++ operation.c
@@ -23,8 +23,33 @@ int get_c(va_list c)
 }
 int get_d(va_list d)
 {
-	int index = va_arg(d, int);
-	int inicio = 0;
+	int n = 987654321;
+	int j = n;
+	int i = 1;
 
+	do
+	{
+		j /= 10;
 
+		if (n / 10 > 1)
+			i++;
+
+	} while (j / 10 != 0);
+
+	return (i);
+}
+int get_i(va_list i)
+{
+	int num = 987654321;
+	int d = num;
+	int i = 1;
+
+	do
+	{
+		d /= 10;
+
+		if (num / 10 > 1)
+			i++;
+		
+	} while (d / 10 != 0);
 }
